48438725ac5b1b92ac29b878f14546aa38d593a8
谢非 <thiefuniverses@gmail.com>
2017-05-31 20:58:07 +0800

    Initial Commit

---
+++ SimpleGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SimpleGL
+//
+//  Created by 谢非 on 2017/5/31.
+//  Copyright © 2017年 谢非. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
